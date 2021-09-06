9489a3f48bf7e155e2ec59ac644a410ba10d055a
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-09-06 17:57:57 +0200

    Push

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 
 var arr = [];
 
-while(arr.length < 5){
+while(arr.length < 3){
     var random = Math.floor(Math.random() * 10) + 1;
 
     if(arr.includes(random) == false);
@@ -16,19 +16,25 @@ alert(arr);
 var i = 0;
 
 var arrNumeri = [];
+var indovinati = [];
 
-setTimeout(secondi, 3000);
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+setTimeout(secondi, 1000);
 function secondi(){
-    for(i = 0; i < 5; i++){
+    for(i = 0; i < 3; i++){
     var numeri = parseInt(prompt("Inserisci i numeri che ricordi uno alla volta."));
     arrNumeri.push(numeri);
     } 
     console.log(arrNumeri);
-}   
 
+    for(var i = 0; i < arrNumeri.length; i++){
+        if(arr.includes(arrNumeri[i]) == true);
+    }  
+}
 
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-
-// Generare 5 numeri casuali e salvarli in un array, poi far partire il timer di 30 secondo dopo aver chiuso l’alert. 
-// Dopo fai partire il set time out di 30 secondi. 
-// Dentro set time out far uscire 5 prompt per inserire i numeri e verificare come da istruzioni.
+   // } else if(arrNumeri.includes(arr) == true){
+    //     arrNumeri.push(arr);
+    //     alert("Hai indovinato i seguenti numeri: " + );
+    // } else 
+    //     alert("Non hai indovinato nessun numero.");
